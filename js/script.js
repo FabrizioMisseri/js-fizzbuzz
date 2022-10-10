c151e9cdaf799e657d5299058336d7a0ca82c612
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-10 13:23:54 +0200

    scritto soluzione all esercizio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 // Scrivi un programma che stampi in console i numeri da 1 a 100,
 //  ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 //  Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+
+//SOLUZIONE
+//contatore da 1 a 100 con ciclo for
+//ad ogni passaggio somma 1 e fai confronto <=100
+//ad ogni iterazione esegui:
+    //condizione se divisibile per 3 scrivi "fizz"
+    //                         per 5 scrivi "buzz"
+    //                         per entrambi "fizzbuzz"
+
+
