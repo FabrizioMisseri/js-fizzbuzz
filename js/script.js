9039d0e175a5070bd140dfd2f066cc31a2de30ef
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-10 13:32:21 +0200

    trasformata soluzione in codice

--- js/script.js
+++ js/script.js
@@ -11,3 +11,21 @@
     //                         per entrambi "fizzbuzz"
 
 
+for (let i = 1; i <= 100; i++) {
+    
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(i, "fizzbuzz");
+    } else {
+        if (i % 3 == 0) {
+            console.log(i, "fizz");
+        }
+        
+        if (i % 5 == 0) {
+            console.log(i, "buzz");
+        }
+    }
+
+    if (i % 3 != 0 && i % 5 != 0) {
+        console.log(i);
+    }
+}
